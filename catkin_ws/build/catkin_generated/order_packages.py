1d6df79c5201ba154ced0cb18fe4b72aeb6b7247
Jvcabrer <jvcabrer@espol.edu.ec>
2024-01-17 01:00:24 -0500

    Not running pt3

--- catkin_ws/build/catkin_generated/order_packages.py
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/jose/Robots final project/catkin_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/jose/Robots final project/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/jose/Robots final project/catkin_ws/devel;/opt/ros/noetic' != '' else []
